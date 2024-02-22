46b4c69bb454263bd7490edd64475c25a270e8d3
JFD <jarroddinwoodie@gmail.com>
2024-02-22 13:46:19 -0800

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,16 +1,16 @@
 <?php
 
 // TABLEAU CREDS
-$username ="jorgeg@naturalskincare.com";
-$password = "Topsecret88";
-$site_url ="cosmetic-solutions-fl";
+$username ="tableau-login-email@example.com";
+$password = "****";
+$site_url ="tableau-site-url-example";
 
 
 $parameters= "?filter.status=All&";
-//$host = 'https://cosmeticsolutionsfl-bieozqsgyuggf5miftz4c3v2mk9yssagqbj.rz-ops.com/api/3.4/auth/signin';
+//$host = 'https://company-name.rz-ops.com/api/3.4/auth/signin';
 $host = 'https://tableau.rz-ops.com/api/3.4/auth/signin';
-$tableau_site_id = 'aa0b49a6-1ff2-4057-9330-60cdfda28b45';
-$tableau_view_id ='f6034bf7-5bbc-4908-b87c-c48c77a8e08a';
+$tableau_site_id = '********';
+$tableau_view_id ='*********';
 /*echo $host.'<br>'; */
 
 
@@ -19,15 +19,13 @@ $config = array(
    // required -------------------------------------
    "endpoint" => "2019_1",
    "host"     => "https://webservices.netsuite.com",
-   //"email"    => "netsuite@naturalskincare.com",
-   //"password" => "Cosmetics2018",
    "role"     => "3",
-   "account"  => "630303",
-   "app_id"   => "1193602D-2295-48AA-AA99-93E1C45EE180",
-   "consumerKey"    => "6b9817ffaa3ba08e027c62829866c7aef1de8aacf96c95db89ac065654846ccc",
-   "consumerSecret" => "13c1946fa20b90b02e8c4c3ef0b282ad5f5c9a99fb84191fa1ec7611e2d19336",
-   "token"          => "1fb5a4b131d9ba7d5e6d1cce41a807d399c9b7f2d7c4e0b566932c3eb50afc77",
-   "tokenSecret"    => "6ad3eaaf7cbe3d2c92a872ab718ee9c11c47a6362b342a9605f02ca453780a7e",
+   "account"  => "company account id -numeric ",
+   "app_id"   => "netsuite app id",
+   "consumerKey"    => "*****",
+   "consumerSecret" => "*****",
+   "token"          => "*****",
+   "tokenSecret"    => "*****",
    // optional -------------------------------------
 );
 ?>
